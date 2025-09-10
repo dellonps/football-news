c668b2dd953ec72123531efe99d5361d5e3edee5
dellonps <deldildolly@gmail.com>
2025-09-10 15:09:35 +0700

    tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","cristian-dillon-footballshop.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","cristian-dillon-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
     "<https://cristian-dillon-footballnews.pbp.cs.ui.ac.id/>"
