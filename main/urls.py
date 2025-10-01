eb3b5a4465df7e498e1d7ee5ebf4d03da1694a48
dellonps <deldildolly@gmail.com>
2025-10-01 14:33:27 +0700

    Final

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main,create_news, show_news, show_news, show_xml, show_json, show_xml_by_id, show_json_by_id,register,login_user,logout_user,edit_news,delete_news
+from main.views import show_main,create_news, show_news, show_news, show_xml, show_json, show_xml_by_id, show_json_by_id,register,login_user,logout_user,edit_news,delete_news,add_news_entry_ajax
 
 app_name = 'main'
 
@@ -16,4 +16,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('create-news-ajax', add_news_entry_ajax, name='add_news_entry_ajax'),
 ]
