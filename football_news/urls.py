735d5e7bfd78034e8077e513a81c840bc4197ee7
dellonps <deldildolly@gmail.com>
2025-11-12 20:58:53 +0700

    tutor 8

--- football_news/urls.py
+++ football_news/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('', include('main.urls')),
+    path('auth/', include('authentication.urls')),
     path('admin/', admin.site.urls),
     
 ]
